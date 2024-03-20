3c3ff724c034d3e2a7bf44ac82c738e9b283def8
Kaif Bailey <kdbailey2003@gmail.com>
2024-03-20 18:32:21 -0400

    Added to the computational essay
    Wrote the intro
    Imported and explained a visualization

--- disc_year_plot.py
+++ disc_year_plot.py
@@ -1,9 +1,21 @@
 from disc_year_data import discovery_year_list
 import matplotlib.pyplot as plt
+import numpy as np
 
-plt.hist(discovery_year_list, bins=250, edgecolor="black")
-plt.title("Number of Exoplanet Discoveries Over the Years")
-plt.xlim(1992, 2024)
-plt.xlabel("Year")
-plt.ylabel("Number of Exoplanets Discovered")
-plt.show()
+
+def plot_disc_year_histogram(start_year, end_year):
+
+    plt.hist(
+        [
+            year
+            for year in discovery_year_list
+            if (year >= start_year) & (year <= end_year)
+        ],
+        bins=30,
+        edgecolor="black",
+    )
+    plt.autoscale(axis="y")
+    plt.title("Number of Exoplanet Discoveries Over the Years")
+    plt.xlabel("Year")
+    plt.ylabel("Number of Exoplanets Discovered")
+    plt.show()
